6143d9d8ee0c5bb3e5fded42f9ae4141eb8cc9e1
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2024-03-07 20:40:54 +0100

    Sidste commit forhåbentlig JA

--- src/main/java/dat3/recipe/api/CategoryController.java
+++ src/main/java/dat3/recipe/api/CategoryController.java
@@ -6,7 +6,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+// hejsa med dig Kenneth
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
